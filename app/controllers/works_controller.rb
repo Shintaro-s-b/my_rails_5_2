dad4b73d253f53f59b29fafd2b4fa6dbb16fb58b
Shintaro-s-b <shintaro_s_b@yahoo.co.jp>
2019-05-30 04:09:29 +0000

    add new routes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,13 @@
 class WorksController < ApplicationController
+
+  skip_before_action :basic_auth, only: [:show, :comment]
+
   def index
     @works = Work.all.order("id ASC")
   end
 
   def show
+    @works = Work.all.order("id ASC")
   end
 
   def upload
@@ -15,8 +19,28 @@ class WorksController < ApplicationController
     redirect_to :action => "index"
   end
 
-  def comment
-    Comment.create({message: params[:message], work_id: params[:work_id]})
+  def add_comment
+    Comment.create({message: params[:message], work_id: params[:work_id]}) if params[:message].present? && params[:work_id].present?
+    redirect_to :action => "show"
+  end
+
+  def reset_comment
+    Work.find(params[:work_id]).comments.delete_all(:delete_all) if params[:work_id].present?
+    redirect_to :action => "index"
+  end
+
+  def set_description
+    Work.find(params[:work_id]).update_attribute(:description, params[:description]) if params[:work_id].present?
+    redirect_to :action => "index"
+  end
+
+  def set_name
+    Work.find(params[:work_id]).update_attribute(:name, params[:name]) if params[:work_id].present?
+    redirect_to :action => "index"
+  end
+
+  def set_url
+    Work.find(params[:work_id]).update_attribute(:url, params[:url]) if params[:work_id].present?
     redirect_to :action => "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'works/index'
   get 'works/show'
   post 'works/upload'
-  post 'works/comment'
+  post 'works/add_comment'
+  post 'works/reset_comment'
+  post 'works/set_description'
+  post 'works/set_name'
+  post 'works/set_url'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
